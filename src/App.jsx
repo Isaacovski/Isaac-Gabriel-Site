147146756ccccffefdf2dafb7eed45a19babea10
Isaac gabriel <116040541+Isaacovski@users.noreply.github.com>
2026-02-21 23:50:18 -0300

    iniciando contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Hero from "./sections/Hero/Hero";
 import Projects from "./sections/Projects/Projects";
+import Skills from "./sections/Skills/Skills";
 
 function App() {
   return (
     <div>
       <Hero></Hero>
       <Projects></Projects>
+      <p></p>
+      <Skills></Skills>
     </div>
   );
 }
